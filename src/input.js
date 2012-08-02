c55cb8d1ffeccda4bdc53ed0f1b8e0db8182f703
Shaun <shaunewilliams@gmail.com>
2012-08-02 01:55:38 -0500

    simplified swipe controls and made more precise

--- src/input.js
+++ src/input.js
@@ -118,133 +118,71 @@ document.onkeyup = function(e) {
 };
 
 var initSwipe = function() {
-    // TOUCH-EVENTS SINGLE-FINGER SWIPE-SENSING JAVASCRIPT
-    // Courtesy of PADILICIOUS.COM and MACOSXAUTOMATION.COM
-    
-    // this script can be used with one or more page elements to perform actions based on them being swiped with a single finger
 
-    var fingerCount = 0;
-    var startX = 0;
-    var startY = 0;
-    var curX = 0;
-    var curY = 0;
-    var deltaX = 0;
-    var deltaY = 0;
-    var horzDiff = 0;
-    var vertDiff = 0;
-    var minLength = 72; // the shortest distance the user may swipe
-    var swipeLength = 0;
-    var swipeAngle = null;
-    var swipeDirection = null;
-    
-    // The 4 Touch Event Handlers
-    
-    // NOTE: the touchStart handler should also receive the ID of the triggering element
-    // make sure its ID is passed in the event call placed in the element declaration, like:
-    // <div id="picture-frame" ontouchstart="touchStart(event,'picture-frame');"  ontouchend="touchEnd(event);" ontouchmove="touchMove(event);" ontouchcancel="touchCancel(event);">
+    // position of anchor
+    var x = 0;
+    var y = 0;
 
-    function touchStart(event) {
-            // disable the standard ability to select the touched object
-            //event.preventDefault();
-            // get the total number of fingers touching the screen
-            fingerCount = event.touches.length;
-            // since we're looking for a swipe (single finger) and not a gesture (multiple fingers),
-            // check that only one finger was used
-            if ( fingerCount == 1 ) {
-                    // get the coordinates of the touch
-                    startX = event.touches[0].pageX;
-                    startY = event.touches[0].pageY;
-            } else {
-                    // more than one finger touched so cancel
-                    touchCancel(event);
-            }
-    }
+    // current distance from anchor
+    var dx = 0;
+    var dy = 0;
 
-    function touchMove(event) {
-            //event.preventDefault();
-            if ( event.touches.length == 1 ) {
-                    curX = event.touches[0].pageX;
-                    curY = event.touches[0].pageY;
-            } else {
-                    touchCancel(event);
-            }
-    }
+    // minimum distance from anchor before direction is registered
+    var r = tileSize;
     
-    function touchEnd(event) {
-            //event.preventDefault();
-            // check to see if more than one finger was used and that there is an ending coordinate
-            if ( fingerCount == 1 && curX != 0 ) {
-                    // use the Distance Formula to determine the length of the swipe
-                    swipeLength = Math.round(Math.sqrt(Math.pow(curX - startX,2) + Math.pow(curY - startY,2)));
-                    // if the user swiped more than the minimum length, perform the appropriate action
-                    if ( swipeLength >= minLength ) {
-                            calculateAngle();
-                            determineSwipeDirection();
-                            processingRoutine();
-                            touchCancel(event); // reset the variables
-                    } else {
-                            touchCancel(event);
-                    }       
-            } else {
-                    touchCancel(event);
-            }
-    }
+    var touchStart = function(event) {
+        var fingerCount = event.touches.length;
+        if (fingerCount == 1) {
 
-    function touchCancel(event) {
-            // reset the variables back to default values
-            fingerCount = 0;
-            startX = 0;
-            startY = 0;
-            curX = 0;
-            curY = 0;
-            deltaX = 0;
-            deltaY = 0;
-            horzDiff = 0;
-            vertDiff = 0;
-            swipeLength = 0;
-            swipeAngle = null;
-            swipeDirection = null;
-    }
-    
-    function calculateAngle() {
-            var X = startX-curX;
-            var Y = curY-startY;
-            var Z = Math.round(Math.sqrt(Math.pow(X,2)+Math.pow(Y,2))); //the distance - rounded - in pixels
-            var r = Math.atan2(Y,X); //angle in radians (Cartesian system)
-            swipeAngle = Math.round(r*180/Math.PI); //angle in degrees
-            if ( swipeAngle < 0 ) { swipeAngle =  360 - Math.abs(swipeAngle); }
-    }
-    
-    function determineSwipeDirection() {
-            if ( (swipeAngle <= 45) && (swipeAngle >= 0) ) {
-                    swipeDirection = 'left';
-            } else if ( (swipeAngle <= 360) && (swipeAngle >= 315) ) {
-                    swipeDirection = 'left';
-            } else if ( (swipeAngle >= 135) && (swipeAngle <= 225) ) {
-                    swipeDirection = 'right';
-            } else if ( (swipeAngle > 45) && (swipeAngle < 135) ) {
-                    swipeDirection = 'down';
-            } else {
-                    swipeDirection = 'up';
-            }
-    }
-    
-    function processingRoutine() {
-            if ( swipeDirection == 'left' ) {
-                pressLeft();
-            } else if ( swipeDirection == 'right' ) {
-                pressRight();
-            } else if ( swipeDirection == 'up' ) {
-                pressUp();
-            } else if ( swipeDirection == 'down' ) {
-                pressDown();
-            }
-    }
+            // commit new anchor
+            x = event.touches[0].pageX;
+            y = event.touches[0].pageY;
 
-    // register touch events with canvas
-    var element = document.getElementById('canvas');
-    element.ontouchstart = touchStart;
-    element.ontouchend = touchEnd;
-    element.ontouchmove = touchMove;
-    element.ontouchcancel = touchCancel;
+        }
+        else {
+            touchCancel(event);
+        }
+    };
+
+    var touchMove = function(event) {
+        var fingerCount = event.touches.length;
+        if (fingerCount == 1) {
+
+            // get current distance from anchor
+            dx = event.touches[0].pageX - x;
+            dy = event.touches[0].pageY - y;
+
+            // if minimum move distance is reached
+            if (dx*dx+dy*dy >= r*r) {
+
+                // commit new anchor
+                x += dx;
+                y += dy;
+
+                // register direction
+                if (Math.abs(dx) >= Math.abs(dy)) {
+                    (dx > 0) ? pressRight() : pressLeft();
+                }
+                else {
+                    (dy > 0) ? pressDown() : pressUp();
+                }
+            }
+        }
+        else {
+            touchCancel(event);
+        }
+    };
+
+    var touchEnd = function(event) {
+    };
+
+    var touchCancel = function(event) {
+        x=y=dx=dy=0;
+    };
+    
+    // register touch events
+    document.ontouchstart = touchStart;
+    document.ontouchend = touchEnd;
+    document.ontouchmove = touchMove;
+    document.ontouchcancel = touchCancel;
 };

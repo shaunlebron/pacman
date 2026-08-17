ec9edc4e8551e96d42d9861cac99a27d299d860d
Shaun Williams <shaunewilliams@gmail.com>
2026-08-16 19:38:32 -0500

    move Button to class

--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,6 @@
 
 // REFERENCED GLOBALS:
 /* global tileSize -- Map.js */
-/* global newChildObject -- inherit.js */
 /* global canvas, renderScale, getDevicePixelRatio, mapMargin -- renderers.js */
 
 const getPointerPos = function(evt) {
@@ -30,120 +29,118 @@ const getPointerPos = function(evt) {
     return { x: mouseX, y: mouseY };
 };
 
-const Button = function(x,y,w,h,onclick) {
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-    this.onclick = onclick;
+class Button {
+    constructor(x,y,w,h,onclick) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.onclick = onclick;
 
-    // text and icon padding
-    this.pad = tileSize;
+        // text and icon padding
+        this.pad = tileSize;
 
 
 
-    // icon attributes
-    this.frame = 0;
+        // icon attributes
+        this.frame = 0;
 
-    this.borderBlurColor = "#333";
-    this.borderFocusColor = "#EEE";
+        this.borderBlurColor = "#333";
+        this.borderFocusColor = "#EEE";
 
-    this.isSelected = false;
+        this.isSelected = false;
 
-    // touch events
-    this.startedInside = false;
-    const that = this;
-    const touchstart = function(evt) {
-        evt.preventDefault();
-        const fingerCount = evt.touches.length;
-        if (fingerCount == 1) {
-            const pos = getPointerPos(evt.touches[0]);
-            (that.startedInside=that.contains(pos.x,pos.y)) ? that.focus() : that.blur();
-        }
-        else {
-            touchcancel(evt);
-        }
-    };
-    const touchmove = function(evt) {
-        evt.preventDefault();
-        const fingerCount = evt.touches.length;
-        if (fingerCount == 1) {
-            if (that.startedInside) {
-                const pos = getPointerPos(evt.touches[0]);
-                that.contains(pos.x, pos.y) ? that.focus() : that.blur();
-            }
-        }
-        else {
-            touchcancel(evt);
-        }
-    };
-    const touchend = function(evt) {
-        evt.preventDefault();
-        const registerClick = (that.startedInside && that.isSelected);
-        if (registerClick) {
-            that.click();
-        }
-        touchcancel(evt);
-        if (registerClick) {
-            // focus the button to keep it highlighted after successful click
-            that.focus();
-        }
-    };
-    const touchcancel = function(evt) {
-        evt.preventDefault();
+        // touch events
         this.startedInside = false;
-        that.blur();
-    };
+        const that = this;
+        const touchstart = function(evt) {
+            evt.preventDefault();
+            const fingerCount = evt.touches.length;
+            if (fingerCount == 1) {
+                const pos = getPointerPos(evt.touches[0]);
+                (that.startedInside=that.contains(pos.x,pos.y)) ? that.focus() : that.blur();
+            }
+            else {
+                touchcancel(evt);
+            }
+        };
+        const touchmove = function(evt) {
+            evt.preventDefault();
+            const fingerCount = evt.touches.length;
+            if (fingerCount == 1) {
+                if (that.startedInside) {
+                    const pos = getPointerPos(evt.touches[0]);
+                    that.contains(pos.x, pos.y) ? that.focus() : that.blur();
+                }
+            }
+            else {
+                touchcancel(evt);
+            }
+        };
+        const touchend = function(evt) {
+            evt.preventDefault();
+            const registerClick = (that.startedInside && that.isSelected);
+            if (registerClick) {
+                that.click();
+            }
+            touchcancel(evt);
+            if (registerClick) {
+                // focus the button to keep it highlighted after successful click
+                that.focus();
+            }
+        };
+        const touchcancel = function(evt) {
+            evt.preventDefault();
+            this.startedInside = false;
+            that.blur();
+        };
 
 
-    // mouse events
-    const click = function(evt) {
-        const pos = getPointerPos(evt);
-        if (that.contains(pos.x, pos.y)) {
-            that.click();
-        }
-    };
-    const mousemove = function(evt) {
-        const pos = getPointerPos(evt);
-        that.contains(pos.x, pos.y) ? that.focus() : that.blur();
-    };
-    const mouseleave = function(evt) {
-        that.blur();
-    };
+        // mouse events
+        const click = function(evt) {
+            const pos = getPointerPos(evt);
+            if (that.contains(pos.x, pos.y)) {
+                that.click();
+            }
+        };
+        const mousemove = function(evt) {
+            const pos = getPointerPos(evt);
+            that.contains(pos.x, pos.y) ? that.focus() : that.blur();
+        };
+        const mouseleave = function(evt) {
+            that.blur();
+        };
 
-    this.isEnabled = false;
-    this.onEnable = function() {
-        canvas.addEventListener('click', click);
-        canvas.addEventListener('mousemove', mousemove);
-        canvas.addEventListener('mouseleave', mouseleave);
-        canvas.addEventListener('touchstart', touchstart);
-        canvas.addEventListener('touchmove', touchmove);
-        canvas.addEventListener('touchend', touchend);
-        canvas.addEventListener('touchcancel', touchcancel);
-        this.isEnabled = true;
-    };
-
-    this.onDisable = function() {
-        canvas.removeEventListener('click', click);
-        canvas.removeEventListener('mousemove', mousemove);
-        canvas.removeEventListener('mouseleave', mouseleave);
-        canvas.removeEventListener('touchstart', touchstart);
-        canvas.removeEventListener('touchmove', touchmove);
-        canvas.removeEventListener('touchend', touchend);
-        canvas.removeEventListener('touchcancel', touchcancel);
-        that.blur();
         this.isEnabled = false;
-    };
-};
+        this.onEnable = function() {
+            canvas.addEventListener('click', click);
+            canvas.addEventListener('mousemove', mousemove);
+            canvas.addEventListener('mouseleave', mouseleave);
+            canvas.addEventListener('touchstart', touchstart);
+            canvas.addEventListener('touchmove', touchmove);
+            canvas.addEventListener('touchend', touchend);
+            canvas.addEventListener('touchcancel', touchcancel);
+            this.isEnabled = true;
+        };
 
-Button.prototype = {
-
-    contains: function(x,y) {
+        this.onDisable = function() {
+            canvas.removeEventListener('click', click);
+            canvas.removeEventListener('mousemove', mousemove);
+            canvas.removeEventListener('mouseleave', mouseleave);
+            canvas.removeEventListener('touchstart', touchstart);
+            canvas.removeEventListener('touchmove', touchmove);
+            canvas.removeEventListener('touchend', touchend);
+            canvas.removeEventListener('touchcancel', touchcancel);
+            that.blur();
+            this.isEnabled = false;
+        };
+    }
+    contains(x,y) {
         return x >= this.x && x <= this.x+this.w &&
-               y >= this.y && y <= this.y+this.h;
-    },
+            y >= this.y && y <= this.y+this.h;
+    }
 
-    click: function() {
+    click() {
         // disable current click timeout (to prevent double clicks on some devices)
         clearTimeout(this.clickTimeout);
 
@@ -152,41 +149,41 @@ Button.prototype = {
         if (that.onclick) {
             this.clickTimeout = setTimeout(function() { that.onclick(); }, 200);
         }
-    },
+    }
 
-    enable: function() {
+    enable() {
         this.frame = 0;
         this.onEnable();
-    },
+    }
 
-    disable: function() {
+    disable() {
         this.onDisable();
-    },
+    }
 
-    focus: function() {
+    focus() {
         this.isSelected = true;
         this.onfocus && this.onfocus();
-    },
+    }
 
-    blur: function() {
+    blur() {
         this.isSelected = false;
         this.onblur && this.onblur();
-    },
+    }
 
-    setText: function(msg) {
+    setText(msg) {
         this.msg = msg;
-    },
+    }
 
-    setFont: function(font,fontcolor) {
+    setFont(font,fontcolor) {
         this.font = font;
         this.fontcolor = fontcolor;
-    },
+    }
 
-    setIcon: function(drawIcon) {
+    setIcon(drawIcon) {
         this.drawIcon = drawIcon;
-    },
+    }
 
-    draw: function(ctx) {
+    draw(ctx) {
 
         // draw border
         ctx.lineWidth = 2;
@@ -226,42 +223,39 @@ Button.prototype = {
             //ctx.fillText(this.msg, 2*tileSize+2*this.pad+this.x, this.y + this.h/2 + 1);
             ctx.fillText(this.msg, this.x + this.w/2, this.y + this.h/2 + 1);
         }
-    },
+    }
 
-    update: function() {
+    update() {
         if (this.drawIcon) {
             this.frame = this.isSelected ? this.frame+1 : 0;
         }
-    },
-};
+    }
+}
 
-const ToggleButton = function(x,y,w,h,isOn,setOn) {
-    const that = this;
-    const onclick = function() {
-        setOn(!isOn());
-        that.refreshMsg();
-    };
-    this.isOn = isOn;
-    this.setOn = setOn;
-    Button.call(this,x,y,w,h,onclick);
-};
-
-ToggleButton.prototype = newChildObject(Button.prototype, {
-
-    enable: function() {
-        Button.prototype.enable.call(this);
+class ToggleButton extends Button {
+    constructor(x,y,w,h,isOn,setOn) {
+        super(x,y,w,h,null);
+        const that = this;
+        this.onclick = function() {
+            setOn(!isOn());
+            that.refreshMsg();
+        };
+        this.isOn = isOn;
+        this.setOn = setOn;
+    }
+    enable() {
+        super.enable();
         this.refreshMsg();
-    },
-    setToggleLabel: function(label) {
+    }
+    setToggleLabel(label) {
         this.label = label;
-    },
-    refreshMsg: function() {
+    }
+    refreshMsg() {
         if (this.label) {
             this.msg = this.label + ": " + (this.isOn() ? "ON" : "OFF");
         }
-    },
-    refreshOnState: function() {
+    }
+    refreshOnState() {
         this.setOn(this.isOn());
-    },
-
-});
+    }
+}

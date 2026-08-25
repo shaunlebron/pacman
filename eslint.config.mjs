51048a29df603391be1d12cd4118699fb0a38790
shaun <shaunewilliams@gmail.com>
2026-08-25 00:11:55 -0500

    fix const assign

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,10 @@ export default defineConfig([
 			"no-undef": "error", // don't use undefined vars (except for /*global*/ refs at top of each file)
 			"no-var": "error", // use let/const instead of var
 			"prefer-const": "error", // use const if not re-assigned
-			// "no-unused-vars": "error",
+			"no-const-assign": "error", // don't reassign a const
+			// "no-redeclare": "error",
+			// "no-global-assign": "error",
+			"no-unused-vars": "off",
 		},
 	},
 ]);

9c932319eeb3e8352bef8233c8b56418ee412514
anupamme <mediratta@gmail.com>
2026-09-02 21:16:37 +0000

    fix: V-001 security vulnerability
    Automated security fix generated by OrbisAI Security

--- promo/server.js
+++ promo/server.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var fs = require('fs');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var csrf = require('csurf');
 
 var app = express();
+app.use(cookieParser());
+app.use(csrf({ cookie: true }));
+
+var framesDir = path.resolve(__dirname, 'frames');
 
 app.post('/frames/:name', function(req,res) {
-	var filename = 'frames/'+req.params.name;
+	var filename = path.resolve(framesDir, req.params.name);
+	if (filename.indexOf(framesDir + path.sep) !== 0) {
+		return res.status(400).send('invalid name');
+	}
 
 	var dataURI = "";
 
